f653e2360bccb8e36ada5d0b7af8e311f75dd355
ryosuke-shibata-hub <ibis.stk@gmail.com>
2024-10-01 23:37:50 +0900

    共通ヘッダー

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,24 +1,34 @@
 <header class="">
     <div class="">
-        <nav class="flex flex-wrap items-center justify-between p-6 bg-teal">
+        <nav class="justify-between p-6 bg-teal">
             <div class="flex items-center mr-6 text-white flex-no-shrink">
-                <img src="{{ asset('static/image/18.png') }}" alt="オカズセレクト" class="mx-auto w-14 h-14">
+                <img src="{{ asset('static/image/18.png') }}" alt="オカズセレクト" class="w-14 h-14">
                 <img src="{{ asset('static/image/site-title.png') }}" alt="おかずせれくと.com" class="-mt-2 w-52" />
             </div>
-            <div class="flex-grow block w-full -mt-2 text-right lg:flex lg:items-center lg:w-auto">
-                <div class="text-lg font-bold text-gray-700 lg:flex-grow">
-                <a href="#responsive-header" class="block mt-4 mr-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-pink-500">
-                    おかずマッチング
-                </a>
-                <a href="#responsive-header" class="block mt-4 mr-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-pink-500">
-                    検索
-                </a>
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-pink-500">
-                    人気のおかず
-                </a>
-                </div>
-                <div>
-                <a href="#" class="inline-block px-4 py-2 mt-4 text-sm leading-none text-white border border-white rounded hover:border-transparent hover:text-teal hover:bg-white lg:mt-0">Download</a>
+            <div class="block w-full text-center lg:items-center lg:w-auto">
+                <div class="text-lg font-bold text-gray-600">
+                    <a
+                        href="/matching"
+                        class="inline-block px-5 mt-4 mr-4 lg:mt-0 text-teal-lighter hover:text-pink-400">
+                        <i class="mr-2 text-pink-600 fa-regular fa-heart"></i>おかずマッチング
+                    </a>
+                    <a
+                        href="/search"
+                        class="inline-block px-5 mt-4 mr-4 lg:mt-0 text-teal-lighter hover:text-blue-400">
+                        <i class="mr-2 text-blue-400 fa-solid fa-magnifying-glass"></i>検索
+                    </a>
+                    <a
+                        href="/ranking"
+                        class="inline-block px-5 mt-4 lg:mt-0 text-teal-lighter hover:text-yellow-400">
+                        <i class="mr-2 text-yellow-400 fa-solid fa-ranking-star"></i>人気のおかず
+                    </a>
+                    <a
+                        href="/help"
+                        class="inline-block px-5 mt-4 lg:mt-0 text-teal-lighter hover:text-gray-900">
+                        <i
+                            class="p-1 px-2 text-gray-800 border-4 rounded-full fa-solid fa-question"
+                            style="color: #929292;"></i>おかずせれくととは
+                    </a>
                 </div>
             </div>
         </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        {{-- fontawesome --}}
+        <script src="https://kit.fontawesome.com/8d671a092c.js" crossorigin="anonymous"></script>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
