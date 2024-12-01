72b0efae58abda26e6927a7f57cd3e5026f471f9
ryosuke-shibata-hub <ibis.stk@gmail.com>
2024-12-01 22:04:00 +0900

    v1.4.1表記

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="block">
         Powered by <a href="https://affiliate.dmm.com/api/" target="_blank">FANZA Webサービス</a>
         <p>&copy; 2024 おかずせれくと.com. All rights reserved.</p>
-        <p class="text-center">ver 1.3.1</p>
+        <p class="text-center">ver 1.4.1</p>
         <a
             href="https://x.com/okazuselect_com"
             class="text-xs font-bold"
